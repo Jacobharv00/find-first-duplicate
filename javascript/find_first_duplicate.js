135439a7a00d15de7c3750a4cf3ee7b0a2521c07
jacob <jacobharv00@gmail.com>
2021-08-12 12:04:00 -0600

    Day two algo

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,6 +1,21 @@
-function findFirstDuplicate(arr) {
-  // type your code here
+function findFirstDuplicate ( arr ) {
+  // loop over the arrays length
+  for ( let i = 0; i < arr.length; i++ ) {
+    // if the value of the last index is not the same return it 
+    if ( arr.indexOf( arr[ i ] ) !== i ) {
+      return arr[i]
+    }
+  }
+  // if none of the numbers are the same return -1
+  return -1
 }
+
+console.log( findFirstDuplicate( [ 1, 2, 3, 3, 2 ] ) )
+console.log( findFirstDuplicate( [ 0, 2, 5, 5, 7, 7, 8, 9] ) )
+console.log( findFirstDuplicate( [ 22, 22, 23, 24, 25, 26] ) )
+console.log( findFirstDuplicate( [ 99, 100, 101, 102, 99, 98] ) )
+console.log( findFirstDuplicate( [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] ) )
+
 
 if (require.main === module) {
   // add your own tests in here
